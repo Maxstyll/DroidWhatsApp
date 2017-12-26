ed379d1ec1432e6b03d56981328ce6a22d411fc8
Robson Raymundo <robson.raymundo@gmail.com>
2017-12-26 19:34:53 -0200

    Initial commit

---
+++ app/src/main/java/ray/droid/com/droidwhatsapp/MainActivity.java
@@ -0,0 +1,13 @@
+package ray.droid.com.droidwhatsapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
