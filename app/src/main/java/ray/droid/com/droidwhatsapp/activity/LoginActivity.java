bf703675893a3cc4de5b3dddb505ed9cc4d1a007
Robson Raymundo <robson.raymundo@gmail.com>
2017-12-27 20:17:48 -0200

    implementando as telas de login e validar sms

---
+++ app/src/main/java/ray/droid/com/droidwhatsapp/activity/LoginActivity.java
@@ -0,0 +1,15 @@
+package ray.droid.com.droidwhatsapp.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import ray.droid.com.droidwhatsapp.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
--- app/src/main/java/ray/droid/com/droidwhatsapp/activity/ValidadorActivity.java
+++ app/src/main/java/ray/droid/com/droidwhatsapp/activity/ValidadorActivity.java
@@ -1,13 +1,15 @@
-package ray.droid.com.droidwhatsapp;
+package ray.droid.com.droidwhatsapp.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import ray.droid.com.droidwhatsapp.R;
+
+public class ValidadorActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_validador);
     }
 }
